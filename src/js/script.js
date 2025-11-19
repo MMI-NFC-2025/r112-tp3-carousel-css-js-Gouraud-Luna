9b434b924db55d6648e2259b48a74636c96a2f7a
Gouraud Luna <luna.gouraud@edu.univ-fcomte.fr>
2025-11-19 09:26:44 +0100

    défilement_largeur_image_etcarrousel_taille_img_responsive

--- src/js/script.js
+++ src/js/script.js
@@ -2,17 +2,18 @@
 const carousel=document.querySelector(".carousel__container")
 const nextButton = document.querySelector(".carousel__button--next")
 const prevButton = document.querySelector(".carousel__button--prev")
+const premierItem = document.querySelector(".carousel__item");
 
 // Largeur de défilement d’un item
 if (carousel) {
     // Scroll au clic sur le bouton précédent
     prevButton.addEventListener("click", () => {
-        carousel.scrollBy({ left: -260, behavior: "smooth" });;
+        carousel.scrollBy({ left: -premierItem.clientWidth, behavior: "smooth" });;
     });
 
     // Scroll au clic sur le bouton suivant
     nextButton.addEventListener("click", () => {
-        carousel.scrollBy({ left: +260, behavior: "smooth" });;
+        carousel.scrollBy({ left: +premierItem.clientWidth, behavior: "smooth" });;
     });
 }
 // TODO
